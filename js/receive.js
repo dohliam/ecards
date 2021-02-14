35dcd69fb5c85b0d6933c2138fa4be0c6cfa9e13
dohliam <dohliam@gmail.com>
2021-02-14 11:57:44 -0800

    receive.js - fix case where no pattern selected

--- js/receive.js
+++ js/receive.js
@@ -1,46 +1,51 @@
+function get_card_object(url) {
+  i = /\?i=([^&]+)/.exec(url);
+  m = /\&m=([^&]+)/.exec(url);
+  c = /\&bc=([^&]+)/.exec(url);
+  p = /\&bp=([^&]+)/.exec(url);
+  r = /\&rb=([^&]+)/.exec(url);
+  s = /\&sb=([^&]+)/.exec(url);
+
+  img = ""
+  if (i) {
+    img = decodeURI(window.atob(i[1]));
+  }
+
+  msg = "";
+  if (m) {
+    msg = decodeURI(window.atob(m[1]));
+  }
+
+  bc = "";
+  if (c) {
+    bc = decodeURI(window.atob(c[1]));
+  }
+
+  bp = "";
+  if (p) {
+    bp = decodeURI(window.atob(p[1]));
+  }
+
+  rb = "";
+  if (r) {
+    rb = decodeURI(window.atob(r[1]));
+  }
+
+  sb = "someone";
+  if (s) {
+    sb = decodeURI(window.atob(s[1]));
+  }
+
+  window.card_image.src = img;
+  card_obj = { "msg":msg, "img":img, "bc":bc, "bp":bp, "r":rb, "s":sb }
+  return card_obj;
+}
+
 function quick_api() {
-  var geturl = location.href;
-  if (/\?/.test(geturl) == true) {
-    if (/\?i=/.test(geturl) == true) {
-      i = /\?i=([^&]+)/.exec(geturl);
-      m = /\&m=([^&]+)/.exec(geturl);
-      c = /\&bc=([^&]+)/.exec(geturl);
-      p = /\&bp=([^&]+)/.exec(geturl);
-      r = /\&rb=([^&]+)/.exec(geturl);
-      s = /\&sb=([^&]+)/.exec(geturl);
-
-      img = ""
-      if (i) {
-        img = decodeURI(window.atob(i[1]));
-      }
-
-      msg = "";
-      if (m) {
-        msg = decodeURI(window.atob(m[1]));
-      }
-
-      bc = "";
-      if (c) {
-        bc = decodeURI(window.atob(c[1]));
-      }
-
-      bp = "";
-      if (p) {
-        bp = decodeURI(window.atob(p[1]));
-      }
-
-      rb = "";
-      if (r) {
-        rb = decodeURI(window.atob(r[1]));
-      }
-
-      sb = "someone";
-      if (s) {
-        sb = decodeURI(window.atob(s[1]));
-      }
-
-      window.card_image.src = img;
-      card_obj = { "msg":msg, "img":img, "bc":bc, "bp":bp, "r":rb, "s":sb }
+  var url = location.href;
+  if (/\?/.test(url) == true) {
+    if (/\?i=/.test(url) == true) {
+      card_obj = get_card_object(url);
       view_card(card_obj);
     }
   }
@@ -55,7 +60,7 @@ function view_card(card_obj) {
   message_preview.innerHTML = card_obj.msg;
   card = document.getElementById("card_container");
   sender.innerHTML = card_obj.s;
-  spacer = ""
+  spacer = "";
   if (!card_obj.r == "") {spacer = " "}
   recipient.innerHTML = spacer + card_obj.r + ", ";
   document.title = card_obj.s + " sent you a card!";
@@ -63,4 +68,7 @@ function view_card(card_obj) {
   bp = "https://raw.githubusercontent.com/mike-hearn/transparent-textures/master/patterns/" + card_obj.bp + ".png";
   card.style.backgroundColor = card_obj.bc;
   card.style.backgroundImage = 'url("' + bp + '")';
+  if (card_obj.bp == "No Background Pattern") {
+    card.style.backgroundImage = "";
+  }
 }
